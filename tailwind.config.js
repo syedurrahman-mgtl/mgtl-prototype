a4265e6c0d8e64b0fb519ac8c0eed63b936d9f12
Syedur Rahman <36609668+syedur-r@users.noreply.github.com>
2023-05-25 18:37:03 +0100

    new content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,20 @@ module.exports = {
       poppins: ["'Poppins'", "sans-serif"],
     },
     screens: {
-      // sm: '480px',
-      sm: '350px',
+      xs: "350px",
       // => @media (min-width: 350px) { ... }
-      
-      md: '768px',
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
       // => @media (min-width: 768px) { ... }
-      
-      lg: '976px',
-      // => @media (min-width: 976px) { ... }
-      
-      xl: '1440px',
-      // => @media (min-width: 1440px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
 
       "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
