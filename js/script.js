537871aa9f5a422fd00c34af501977fba4502f08
Syedur Rahman <36609668+syedur-r@users.noreply.github.com>
2023-05-31 17:27:01 +0100

    new content

--- js/script.js
+++ js/script.js
@@ -1,40 +1,49 @@
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
+// Get the button
+let scrollerBtn = document.querySelector(".btnBackToTop");
 
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+// // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    scrollerBtn.style.display = "block";
+  } else {
+    scrollerBtn.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+scrollerBtn.addEventListener("click", backToTop);
 
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  document.body.style.scrollBehavior = 'smooth';
+}
 
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-});
+// // Burger menus
+// document.addEventListener('DOMContentLoaded', function() {
+//     // open and close
+//     const burger = document.querySelectorAll('.navbar-burger');
+//     const menu = document.querySelectorAll('.navbar-menu');
+
+//     if (burger.length && menu.length) {
+//         for (var i = 0; i < burger.length; i++) {
+//             burger[i].addEventListener('click', function() {
+//                 for (var j = 0; j < menu.length; j++) {
+//                     menu[j].classList.toggle('hidden');
+//                 }
+//             });
+//         }
+//     }
+// });
+
+
+function toggleSlideover() {
+    document.getElementById('slideover-container').classList.toggle('invisible');
+    document.getElementById('slideover').classList.toggle('translate-x-full');
+}
